c460624433afe9b27bb787e700c793b3b7bdb88b
SelvaGaneshM <selvaganesh.developer@gmail.com>
2020-01-02 15:32:36 +0530

    initial commit

---
+++ app/src/main/java/com/selvaganesh/karadipathinterview/splash/MainActivity.java
@@ -0,0 +1,16 @@
+package com.selvaganesh.karadipathinterview.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.selvaganesh.karadipathinterview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
